b0a9c32830933b356a9cc0337391e69f934e7132
Maciej Gierałtowski <mgieraltowski02@gmail.com>
2024-12-30 23:37:20 +0100

    add vercel integration

--- server/app.ts
+++ server/app.ts
@@ -11,5 +11,4 @@ app.route("/api/expenses", expensesRoute);
 
 app.get("*", serveStatic({ root: "./frontend/dist" }));
 app.get("*", serveStatic({ path: "./frontend/dist/index.html" }));
-
 export default app;
